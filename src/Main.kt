62777fe37330f54c324e43bb5a3af1dee012a294
scar <scarletmacaw73@gmail.com>
2025-05-13 09:33:53 -0400

    Created First time setup function.

--- src/Main.kt
+++ src/Main.kt
@@ -1,5 +1,19 @@
+import java.io.File
+
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
+firstSetup()
+}
 
+fun firstSetup(){
+    print("What Is your File Path? ")
+    var outDir = File(readln())
+    while (!outDir.isDirectory || !outDir.exists()) {
+        println("Path does not exist, or is not a valid directory, please input a new path")
+        outDir = File(readln())
+    }
+    val tempOutPath = File("./output.txt")
+    tempOutPath.writeText(outDir.getAbsolutePath())
 }
