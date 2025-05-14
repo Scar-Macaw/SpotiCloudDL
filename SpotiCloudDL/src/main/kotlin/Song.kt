bf9417b30fd7bbc38ab8931bbd15ddf01fbf7d09
scar <scarletmacaw73@gmail.com>
2025-05-13 21:58:00 -0400

    trying to migrate to gradle idk

---
+++ SpotiCloudDL/src/main/kotlin/Song.kt
@@ -0,0 +1,13 @@
+class Song {
+    var name: String = ""
+    var album: String = ""
+    var artist: String = ""
+    var genre: String = ""
+    var trackNo: Int = 0
+    var year: Int = 0
+    var discNo: Int = 0
+
+    fun getInfo(url: String){
+        //api call url
+    }
+}
